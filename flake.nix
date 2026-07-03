cc2fb1571d2239ed7425607404711a69eddbf9cb
Allspike <justin@spikerman.net>
2026-07-02 19:56:38 -0700

    Added flakes to git

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Justin's Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }@inputs: {
+    nixosConfigurations = {
+      justin-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
