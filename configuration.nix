068330d6d87eb8dab8688deccce047114d2d1602
Allspike <justin@spikerman.net>
2026-07-05 12:49:48 -0700

    cleaned up programs variable

--- configuration.nix
+++ configuration.nix
@@ -102,22 +102,28 @@
   # Install firefox.
   programs.firefox.enable = true;
 
+  programs = {
+    firefox.enable = true;
+
   # Install Steam
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    package = pkgs.steam.override {
-      extraEnv = {
-        MANGOHUD = "1";
-        MANGOHUD_CONFIG = "read_cfg,no_display";
-        GAMEMODERUN = "1";
-        PROTON_FSR_UPGRADE = "1";
-        PROTON_ENABLE_WAYLAND = "1";
-        DXVK_HDR = "1";
-        ENABLE_LAYER_MESA_ANTI_LAG = "1";
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      package = pkgs.steam.override {
+        extraEnv = {
+          MANGOHUD = "1";
+          MANGOHUD_CONFIG = "read_cfg,no_display";
+          GAMEMODERUN = "1";
+          PROTON_FSR_UPGRADE = "1";
+          PROTON_ENABLE_WAYLAND = "1";
+          DXVK_HDR = "1";
+          ENABLE_LAYER_MESA_ANTI_LAG = "1";
+        };
       };
-    };
+    }
+
+    fish.enable = true;
   };
 
   programs.gamemode.enable = true; # performance mode
