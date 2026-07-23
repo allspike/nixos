5d2958786ef75cb6b9f3989f1c59d4a724ab2e55
Allspike <jspikerman@icloud.com>
2026-07-23 09:32:42 -0700

    added optiscaler environment variable

--- modules/programs/gaming.nix
+++ modules/programs/gaming.nix
@@ -12,6 +12,7 @@
         MANGOHUD = "1";
         MANGOHUD_CONFIG = "read_cfg,no_display";
         GAMEMODERUN = "1";
+        PROTON_USE_OPTISCALER=1;
         PROTON_FSR_UPGRADE = "1";
         PROTON_ENABLE_WAYLAND = "1";
         DXVK_HDR = "1";
