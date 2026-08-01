bfaed06c1e66e03b2e7f156d3fc5e78b30f6cce1
Allspike <jspikerman@icloud.com>
2026-08-01 10:21:48 -0700

    added tailscale config to networking module

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,7 +1,23 @@
 
 {
+  services.tailscale = {
+    enable = true;
+  };
   networking = {
     hostName = "justin-nixos";
     networkmanager.enable = true;
+    nftables.enable = true;
+    firewall = {
+      true;
+      trustedInterfaces = [ config.services.tailscale.interfaceName ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+    };
   };
+
+  systemd.services.tailscaled.serviceConfig.Environment = [
+    "TS_DEBUG_FIREWALL_MODE=nftables"
+  ];
+
+  systemd.network.wait-online.enable = false;
+  boot.initrd.systemd.network.wait-online.enable = false;
 }
