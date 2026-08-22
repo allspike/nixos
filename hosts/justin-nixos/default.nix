db4b6ad64ff57c7f5aff3bec5d0a38fdb4261619
Allspike <jspikerman@icloud.com>
2026-08-22 14:09:56 -0700

    updated flake inputs and added nh

--- hosts/justin-nixos/default.nix
+++ hosts/justin-nixos/default.nix
@@ -11,6 +11,7 @@
     ../../modules/system/nix.nix
     ../../modules/system/packages.nix
     ../../modules/system/power.nix
+    ../../modules/system/nh.nix
 
     #../../modules/desktop/plasma.nix
     #../../modules/desktop/kde_overlay.nix
--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -6,5 +6,7 @@
     fzf
     nvme-cli
     btop
+
   ];
+
 }
