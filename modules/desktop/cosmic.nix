5d65a03cc2ddbfde340806d47d45aa87e132abf0
Allspike <jspikerman@icloud.com>
2026-08-24 15:10:48 -0700

    updated cosmic settings

--- modules/desktop/cosmic.nix
+++ modules/desktop/cosmic.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }:
 
 {
-  services.displayManager.cosmic-greeter.enable = true;
+  services = {
+    displayManager.cosmic-greeter.enable = true;
+    desktopManager.cosmic.enable = true;
+    displayManager.autoLogin = {
+      enable = true;
+      user = "justins";
+    };
+    system76-scheduler.enable = true;
+  };
 
-  services.desktopManager.cosmic.enable = true;
-
-
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "justins";
+  programs.firefox.preferences = {
+    #disable libadwaita theming for Firefox
+    "widget.gtk.libadwaita-colors.enable" = false;
   };
 }
